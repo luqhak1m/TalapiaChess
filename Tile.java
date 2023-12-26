bd0729225e4a574a5eb05964a65d19ed8c64a9aa
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-26 21:52:32 +0800

    Update Tile.java
    added listener to each tile

--- Tile.java
+++ Tile.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Tile extends JButton{
 
@@ -7,10 +8,15 @@ public class Tile extends JButton{
     
     public static Tile[][] tiles = new Tile[6][7];
 
-    public Tile(int x, int y){
+    public Tile(int x, int y, GameControl c){
         setBackground(new Color(139,68,19));
         setSize(200, 200);  
         this.xCoord=x;
-        this.yCoord=y;               
+        this.yCoord=y;     
+        this.addMouseListener(new MouseAdapter(){
+            public void mouseClicked(MouseEvent e){
+                c.clickTile(xCoord, yCoord, c);
+            }
+        });          
     }
 }
