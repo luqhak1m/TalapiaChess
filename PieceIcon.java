b1d8927efc96d2018534d1128edf0066628bc887
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-26 21:19:48 +0800

    Update PieceIcon.java
    removed unnecessary methods from the View class

--- PieceIcon.java
+++ PieceIcon.java
@@ -5,8 +5,6 @@ import javax.swing.ImageIcon;
 public class PieceIcon {
     private Piece piece;
     private ImageIcon pieceImage;
-    public static PieceIcon[][] piecesIcons = new PieceIcon[6][7];
-
 
     public static ImageIcon getImage(String path){
         ImageIcon pieceImage = new ImageIcon(path);
@@ -17,21 +15,15 @@ public class PieceIcon {
         return pieceImage;
     }
 
-    public void setIconAtPosition(int x, int y){
-
-        Tile.tiles[x][y].setIcon(null);
-        Tile.tiles[piece.getPosX()][piece.getPosY()].setIcon(pieceImage);
-    }
-
     public Piece getPiece(){
         return this.piece;
+    }
+    public ImageIcon getImg(){
+        return this.pieceImage;
     }
 
     public PieceIcon(Piece p, ImageIcon img){
         this.piece=p;
         this.pieceImage=img;
-
-        Tile.tiles[p.getPosX()][p.getPosY()].setIcon(img);
-        piecesIcons[p.getPosX()][p.getPosY()]=this;
     }
 }
