43cad86fce278c83f0f8d578b66b6b0538a734d3
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-26 21:18:52 +0800

    Update Main.java
    adapt to the new mapping system

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        GameControl controller=new GameControl();
         new Board();
 
         PlusPiece yellowPlus1 = new PlusPiece(5, 0);
@@ -18,9 +19,9 @@ public class Main {
         SunPiece blueSun = new SunPiece(0, 3);
         PointPiece[] yellowPoints = new PointPiece[7];
         PointPiece[] bluePoints = new PointPiece[7];
-        for (int col = 0; col < 7; col++) {
-            yellowPoints[col] = new PointPiece(4, col);
-            bluePoints[col] = new PointPiece(1, col);
+        for (int column = 0; column < 7; column++) {
+            yellowPoints[column] = new PointPiece(4, column);
+            bluePoints[column] = new PointPiece(1, column);
         }
 
         PieceIcon bluePlus1Icon = new PieceIcon(bluePlus1, PieceIcon.getImage("piecesPics/bluePlus.png"));
@@ -32,9 +33,9 @@ public class Main {
         PieceIcon blueSunIcon = new PieceIcon(blueSun, PieceIcon.getImage("piecesPics/blueSun.png"));
         PieceIcon[] blueArrowIcons = new PieceIcon[7];
         PieceIcon[] yellowArrowIcons = new PieceIcon[7];
-        for (int col = 0; col < 7; col++) {
-            blueArrowIcons[col] = new PieceIcon(bluePoints[col], PieceIcon.getImage("piecesPics/blueArrow.png"));
-            yellowArrowIcons[col] = new PieceIcon(yellowPoints[col], PieceIcon.getImage("piecesPics/yellowArrow.png"));
+        for (int column=0; column<7; column++) {
+            blueArrowIcons[column] = new PieceIcon(bluePoints[column], PieceIcon.getImage("piecesPics/blueArrow.png"));
+            yellowArrowIcons[column] = new PieceIcon(yellowPoints[column], PieceIcon.getImage("piecesPics/yellowArrow.png"));
         }
         PieceIcon yellowPlus1Icon = new PieceIcon(yellowPlus1, PieceIcon.getImage("piecesPics/yellowPlus.png"));
         PieceIcon yellowPlus2Icon = new PieceIcon(yellowPlus2, PieceIcon.getImage("piecesPics/yellowPlus.png"));
@@ -43,9 +44,50 @@ public class Main {
         PieceIcon yellowTime1Icon = new PieceIcon(yellowTime1, PieceIcon.getImage("piecesPics/yellowTime.png"));
         PieceIcon yellowTime2Icon = new PieceIcon(yellowTime2, PieceIcon.getImage("piecesPics/yellowTime.png"));
         PieceIcon yellowSunIcon = new PieceIcon(yellowSun, PieceIcon.getImage("piecesPics/yellowSun.png"));
-        
 
-        Piece.getPiecesPositionAtIndex(0, 0).setPosXY(5, 6); //
-        PieceIcon.piecesIcons[0][0].setIconAtPosition(0,0);
+        controller.mapPiecesIcon(bluePlus1, bluePlus1Icon);
+        controller.mapPiecesIcon(bluePlus2, bluePlus2Icon);
+        controller.mapPiecesIcon(blueHourglass1, blueHourglass1Icon);
+        controller.mapPiecesIcon(blueHourglass2, blueHourglass2Icon);
+        controller.mapPiecesIcon(blueTime1, blueTime1Icon);
+        controller.mapPiecesIcon(blueTime2, blueTime2Icon);
+        controller.mapPiecesIcon(blueSun, blueSunIcon);
+        for (int column = 0; column < 7; column++) {
+            controller.mapPiecesIcon(bluePoints[column], blueArrowIcons[column]);
+        }
+
+        controller.mapPiecesIcon(yellowPlus1, yellowPlus1Icon);
+        controller.mapPiecesIcon(yellowPlus2, yellowPlus2Icon);
+        controller.mapPiecesIcon(yellowHourglass1, yellowHourglass1Icon);
+        controller.mapPiecesIcon(yellowHourglass2, yellowHourglass2Icon);
+        controller.mapPiecesIcon(yellowTime1, yellowTime1Icon);
+        controller.mapPiecesIcon(yellowTime2, yellowTime2Icon);
+        controller.mapPiecesIcon(yellowSun, yellowSunIcon);
+        for (int column = 0; column < 7; column++) {
+            controller.mapPiecesIcon(yellowPoints[column], yellowArrowIcons[column]);
+        }
+
+        // Setting the icons for each Piece on the board
+        controller.setPiecesIcon(bluePlus1);
+        controller.setPiecesIcon(bluePlus2);
+        controller.setPiecesIcon(blueHourglass1);
+        controller.setPiecesIcon(blueHourglass2);
+        controller.setPiecesIcon(blueTime1);
+        controller.setPiecesIcon(blueTime2);
+        controller.setPiecesIcon(blueSun);
+        for (int column = 0; column < 7; column++) {
+            controller.setPiecesIcon(bluePoints[column]);
+        }
+
+        controller.setPiecesIcon(yellowPlus1);
+        controller.setPiecesIcon(yellowPlus2);
+        controller.setPiecesIcon(yellowHourglass1);
+        controller.setPiecesIcon(yellowHourglass2);
+        controller.setPiecesIcon(yellowTime1);
+        controller.setPiecesIcon(yellowTime2);
+        controller.setPiecesIcon(yellowSun);
+        for (int column = 0; column < 7; column++) {
+            controller.setPiecesIcon(yellowPoints[column]);
+        }
     }
 }
