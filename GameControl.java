c4969c288e40fb2f77f6df4f6f518f365152cc5d
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-26 21:54:49 +0800

    Update GameControl.java
    updated control to handle the clicking

--- GameControl.java
+++ GameControl.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 
 public class GameControl {
     private HashMap<Piece, PieceIcon> pieceIconMap = new HashMap<>();
+    Piece piecePositions[][]=new Piece[6][7];
     
     public void mapPiecesIcon(Piece p, PieceIcon pIcon){
         pieceIconMap.putIfAbsent(p, pIcon);
@@ -12,7 +13,24 @@ public class GameControl {
         return pieceIconMap;
     }
 
+    public void clickTile(int x, int y, GameControl c){
+        if(Piece.piecePositions[x][y]!=null){
+            Piece.selectedPiece=Piece.piecePositions[x][y];
+            System.out.println(Piece.selectedPiece);
+        }else{
+            System.out.println("tile is null");
+        }
+    }
+
     public void setPiecesIcon(Piece p){
         Tile.tiles[p.getPosX()][p.getPosY()].setIcon(pieceIconMap.get(p).getImg());
+    }
+
+    public void setPiecesPositions(int x, int y, Piece p){
+        Piece.piecePositions[x][y]=p;
+    }
+
+    public Piece getPieceAtPosition(int x, int y){
+        return Piece.piecePositions[x][y];
     }
 }
