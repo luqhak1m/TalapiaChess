41e28bc5fb200b7e22d3e9a23013b67acb4c398d
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-30 21:26:38 +0800

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,23 +1,64 @@
-import javax.swing.JFrame;
-import javax.swing.*;
-import java.awt.*;
 
-public class Board extends JFrame{
+import java.util.HashMap;
 
-    JPanel gameBoard = new JPanel(new GridLayout (6,7));
+public class GameControl {
+    private HashMap<Piece, PieceIcon> pieceIconMap = new HashMap<>();
+    Piece piecePositions[][]=new Piece[6][7];
+    
+    public void mapPiecesIcon(Piece p, PieceIcon pIcon){
+        pieceIconMap.putIfAbsent(p, pIcon);
+    }
 
-    public Board(GameControl c){
+    public HashMap<Piece, PieceIcon> getMap(){
+        return pieceIconMap;
+    }
 
-        for(int i=0; i<6; i++){
-            for(int j=0; j<7; j++){
-                    Tile.tiles[i][j] = new Tile(i, j, c);
-                    gameBoard.add(Tile.tiles[i][j]);
-            }
+    public void clickTile(int x, int y){
+
+        System.out.print("Tile clicked. ");
+
+        if(Piece.selectedPiece!=null){
+            System.out.print("Placed piece: ");
+            movePieces(x, y, Piece.selectedPiece);
+        }else{
+            selectPiece(Piece.piecePositions[x][y]);
+            System.out.println("Selected piece: " + Piece.selectedPiece);
         }
 
-        add(gameBoard, BorderLayout.CENTER);
-        setSize(600,600);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // if(Piece.piecePositions[x][y]!=null){
+        //     Piece.selectedPiece=Piece.piecePositions[x][y];
+        //     Piece.selectedPiece.printSomething();
+        // }else{
+        //     System.out.println("tile is null");
+        // }
+    }
+
+    public void setPiecesIcon(Piece p){
+        Tile.tiles[p.getPosX()][p.getPosY()].setIcon(pieceIconMap.get(p).getImg());
+    }
+
+    public void removePiecesIcon(int x, int y){
+        Tile.tiles[x][y].setIcon(null);
+    }
+
+    public void setPiecesPositions(int x, int y, Piece p){
+        Piece.piecePositions[x][y]=p;
+    }
+
+    public void selectPiece(Piece p){
+        Piece.selectedPiece=p;
+    }
+
+    public void movePieces(int x, int y, Piece p){
+        removePiecesIcon(p.getPosX(), p.getPosY());
+        p.setPosXY(x, y);
+        setPiecesPositions(x, y, p);
+        setPiecesIcon(p);
+        Piece.selectedPiece.printSomething();
+        Piece.selectedPiece=null;
+    }
+
+    public Piece getPieceAtPosition(int x, int y){
+        return Piece.piecePositions[x][y];
     }
 }
