4d3f51a718561bcf5ad8d44916713d5c644a2032
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-30 21:35:37 +0800

    Update GameControl.java
    movement

--- GameControl.java
+++ GameControl.java
@@ -24,13 +24,6 @@ public class GameControl {
             selectPiece(Piece.piecePositions[x][y]);
             System.out.println("Selected piece: " + Piece.selectedPiece);
         }
-
-        // if(Piece.piecePositions[x][y]!=null){
-        //     Piece.selectedPiece=Piece.piecePositions[x][y];
-        //     Piece.selectedPiece.printSomething();
-        // }else{
-        //     System.out.println("tile is null");
-        // }
     }
 
     public void setPiecesIcon(Piece p){
@@ -41,9 +34,13 @@ public class GameControl {
         Tile.tiles[x][y].setIcon(null);
     }
 
+    public void removePiecesPositions(int x, int y){
+        Piece.piecePositions[x][y]=null;
+    }
     public void setPiecesPositions(int x, int y, Piece p){
         Piece.piecePositions[x][y]=p;
     }
+
 
     public void selectPiece(Piece p){
         Piece.selectedPiece=p;
@@ -51,6 +48,7 @@ public class GameControl {
 
     public void movePieces(int x, int y, Piece p){
         removePiecesIcon(p.getPosX(), p.getPosY());
+        removePiecesPositions(p.getPosX(), p.getPosY());
         p.setPosXY(x, y);
         setPiecesPositions(x, y, p);
         setPiecesIcon(p);
