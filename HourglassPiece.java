ba7bdf7411af4753caf685461377db4b011e0d60
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-30 21:25:40 +0800

    Update HourglassPiece.java
    abstract method

--- HourglassPiece.java
+++ HourglassPiece.java
@@ -3,5 +3,10 @@ public class HourglassPiece extends Piece{
     public HourglassPiece(int x, int y) {
         super(x, y);
     }
+
+    @Override
+    public void printSomething() {
+        System.out.println("I am hourglass");
+    }
     
 }
