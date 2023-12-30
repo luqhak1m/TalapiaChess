6c92e48d6b83e64451b3cfa3d4984192a26c3673
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-30 21:24:15 +0800

    Update TimePiece.java
    abstract class

--- TimePiece.java
+++ TimePiece.java
@@ -3,5 +3,10 @@ public class TimePiece extends Piece{
     public TimePiece(int x, int y) {
         super(x, y);
     }
+
+    @Override
+    public void printSomething() {
+        System.out.println("I am time");
+    }
     
 }
