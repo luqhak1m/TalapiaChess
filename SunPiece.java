2150b834730d6c24cd4bc6f6712d68b89795c3fc
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-30 21:24:40 +0800

    Update SunPiece.java
    abstract method

--- SunPiece.java
+++ SunPiece.java
@@ -3,5 +3,10 @@ public class SunPiece extends Piece{
     public SunPiece(int x, int y) {
         super(x, y);
     }
+
+    @Override
+    public void printSomething() {
+        System.out.println("I am sun");
+    }
     
 }
