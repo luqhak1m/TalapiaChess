b261941275297e1fa7e522b5d158a88ca571e420
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-30 21:24:57 +0800

    Update PointPiece.java
    abstract method

--- PointPiece.java
+++ PointPiece.java
@@ -3,5 +3,10 @@ public class PointPiece extends Piece{
     public PointPiece(int x, int y) {
         super(x, y);
     }
+
+    @Override
+    public void printSomething() {
+        System.out.println("I am point");
+    }
     
 }
