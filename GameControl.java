ef0f1d7729638b5a2156c623fc91268b69b00e63
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-12-30 21:27:04 +0800

    Update GameControl.java

--- GameControl.java
+++ GameControl.java
@@ -13,21 +13,49 @@ public class GameControl {
         return pieceIconMap;
     }
 
-    public void clickTile(int x, int y, GameControl c){
-        if(Piece.piecePositions[x][y]!=null){
-            Piece.selectedPiece=Piece.piecePositions[x][y];
-            System.out.println(Piece.selectedPiece);
+    public void clickTile(int x, int y){
+
+        System.out.print("Tile clicked. ");
+
+        if(Piece.selectedPiece!=null){
+            System.out.print("Placed piece: ");
+            movePieces(x, y, Piece.selectedPiece);
         }else{
-            System.out.println("tile is null");
+            selectPiece(Piece.piecePositions[x][y]);
+            System.out.println("Selected piece: " + Piece.selectedPiece);
         }
+
+        // if(Piece.piecePositions[x][y]!=null){
+        //     Piece.selectedPiece=Piece.piecePositions[x][y];
+        //     Piece.selectedPiece.printSomething();
+        // }else{
+        //     System.out.println("tile is null");
+        // }
     }
 
     public void setPiecesIcon(Piece p){
         Tile.tiles[p.getPosX()][p.getPosY()].setIcon(pieceIconMap.get(p).getImg());
     }
 
+    public void removePiecesIcon(int x, int y){
+        Tile.tiles[x][y].setIcon(null);
+    }
+
     public void setPiecesPositions(int x, int y, Piece p){
         Piece.piecePositions[x][y]=p;
+    }
+
+    public void selectPiece(Piece p){
+        Piece.selectedPiece=p;
+    }
+
+    public void movePieces(int x, int y, Piece p){
+        removePiecesIcon(p.getPosX(), p.getPosY());
+        p.setPosXY(x, y);
+        setPiecesPositions(x, y, p);
+        setPiecesIcon(p);
+        Piece.selectedPiece.printSomething();
+        Piece.selectedPiece=null;
     }
 
     public Piece getPieceAtPosition(int x, int y){
