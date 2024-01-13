619ec378905e235ea698e038f9f19cbfbcf4d201
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2024-01-13 17:25:12 +0800

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,15 +1,17 @@
 public abstract class Piece{
 
     private int posX, posY;
-    private char side;
+    private char side, status;
     static Piece selectedPiece=null;
     static Piece piecePositions[][]=new Piece[6][7];
 
 
-    public Piece(int x, int y){
+    public Piece(int x, int y, char st, char si){
 
         this.posX=x;
         this.posY=y;
+        this.status=st;
+        this.side=si;
 
         if(x==0||x==1){
             this.side='B';
@@ -26,11 +28,29 @@ public abstract class Piece{
     public int getPosY(){
         return this.posY;
     }
+    public char getStatus(){
+        return status;
+    }
+    public char getSide(){
+        return side;
+    }
 
     public void setPosXY(int x, int y){
         this.posX=x;
         this.posY=y;
     }
 
-    public abstract void printSomething();
+    public boolean emptyTile(int x, int y){
+        return Tile.tiles[x][y]==null;
+    }
+
+    public void setStatusDead(){
+        this.status='D';
+    }
+    public void setStatusAlive(){
+        this.status=1;
+    }
+
+    public abstract boolean validMove(int newX, int newY);
+    public abstract boolean skipPiece(int newX, int newY); // will check for piece skip in the validmove
 }
