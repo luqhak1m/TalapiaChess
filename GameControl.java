f8eba29e72868632226aed3569f2790d20b51552
Luqman <1211101583@student.mmu.edu.my>
2024-01-15 01:05:19 +0800

    Deleted swappingpieceinfo class whatever that was

--- GameControl.java
+++ GameControl.java
@@ -7,21 +7,16 @@ import java.util.Map;
 public class GameControl {
     private int turnCount=0;
     private char whoseTurn='Y';
-    private Map<Class<? extends Piece>, SwappingPieceInfo> swapPieceMap=new HashMap<>();
+    private Map<Class<? extends Piece>, Class<? extends Piece>> swapPieceMapTest=new HashMap<>();
 
     public GameControl(){
-
         pieceIconMap.putIfAbsent(PointPiece.class, new PieceIcon(PointPiece.class, PieceIcon.getImage("piecesPics/yellowArrow.png"), PieceIcon.getImage("piecesPics/blueArrow.png")));
         pieceIconMap.putIfAbsent(PlusPiece.class, new PieceIcon(PlusPiece.class, PieceIcon.getImage("piecesPics/yellowPlus.png"), PieceIcon.getImage("piecesPics/bluePlus.png")));
         pieceIconMap.putIfAbsent(TimePiece.class, new PieceIcon(TimePiece.class, PieceIcon.getImage("piecesPics/yellowTime.png"), PieceIcon.getImage("piecesPics/blueTime.png")));
         pieceIconMap.putIfAbsent(HourglassPiece.class, new PieceIcon(HourglassPiece.class, PieceIcon.getImage("piecesPics/yellowHourglass.png"), PieceIcon.getImage("piecesPics/blueHourglass.png")));
         pieceIconMap.putIfAbsent(SunPiece.class, new PieceIcon(SunPiece.class, PieceIcon.getImage("piecesPics/yellowSun.png"), PieceIcon.getImage("piecesPics/blueSun.png")));
 
-        System.out.println(pieceIconMap);
-
-        swapPieceMap.put(TimePiece.class, new SwappingPieceInfo(PlusPiece.class, "piecesPics/yellowPlus.png", "piecesPics/bluePlus.png"));
-        swapPieceMap.put(PlusPiece.class, new SwappingPieceInfo(TimePiece.class, "piecesPics/yellowTime.png", "piecesPics/blueTime.png"));
-        swapPieceMap.put(SunPiece.class, new SwappingPieceInfo(PointPiece.class, "piecesPics/yellowArrow.png", "piecesPics/blueArrow.png"));
+        swapPieceMapTest.put(SunPiece.class, PointPiece.class);
     }
 
     // mapping Pieces to its respective pieceIcons
@@ -96,12 +91,10 @@ public class GameControl {
         for(int i=0; i<Piece.row; i++){
             for(int j=0; j<Piece.column; j++){
                 Piece currentPiece=Piece.piecePositions[i][j];
-                if(currentPiece!=null && swapPieceMap.containsKey(currentPiece.getClass())){ // if it's a subclass of the mapped object
-                    SwappingPieceInfo toBeSwappedObj=swapPieceMap.get(currentPiece.getClass()); // get the mapped object
-                    Class<? extends Piece> toBeSwappedSubclass=toBeSwappedObj.getPieceType(); // get the subclass of the object
-                    Piece toBeSwapped=CreatePiece.createPiece(toBeSwappedSubclass.getSimpleName(), i, j, 'A', currentPiece.getSide());
+                if(currentPiece!=null && swapPieceMapTest.containsKey(currentPiece.getClass())){ // if the piece's class is in the map of the pieces to be swapped
+                    Piece latestPiece=CreatePiece.createPiece(swapPieceMapTest.get(currentPiece.getClass()).getSimpleName(), i, j, 'A', currentPiece.getSide()); // create the new piece based on the map pairing
                     removePieceFromTile(currentPiece);
-                    setPieceAtTile(toBeSwapped);
+                    setPieceAtTile(latestPiece);
                 }
             }
         }
