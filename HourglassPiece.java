b1bda7464b3a455540ae936655bb63cd7546a86d
Luqman <1211101583@student.mmu.edu.my>
2024-01-18 01:17:43 +0800

    Hourglass Movement

--- HourglassPiece.java
+++ HourglassPiece.java
@@ -1,4 +1,3 @@
-
 // Model
 
 public class HourglassPiece extends Piece{
@@ -9,12 +8,22 @@ public class HourglassPiece extends Piece{
 
     @Override
     public boolean validMove(int newX, int newY) {
-        return true;
+        int hDistance = Math.abs(newX - getPosX()); //horizontal distance
+        int vDistance = Math.abs(newY - getPosY()); //vertical distance
+        System.out.println("hDistance: " + hDistance + ", vDistance: " + vDistance);
+
+        //movement of hourglass piece in 3x2 L shape in any orientation
+        return (hDistance == 2 && vDistance == 1) || (hDistance == 3 && vDistance == 1) || (hDistance == 1 && vDistance == 2); //return true if 3x2 in L shape
+        
     }
 
     @Override
     public boolean skipPiece(int newX, int newY) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'skipPiece'");
+        //check if next coordinates are within the board bounds
+        if (newX < 0 || newX >= Piece.row || newY < 0 || newY >= Piece.column) {
+            return false;
+        }
+
+        return true; //skips over any piece
     }
 }
