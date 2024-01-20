9dbaebbc19acd6bd2fa773f03046dc89359d19ee
TENGKUALYSSASABRINA <108215206+TENGKUALYSSASABRINA@users.noreply.github.com>
2024-01-20 08:07:04 +0800

    Update PlusPiece.java

--- PlusPiece.java
+++ PlusPiece.java
@@ -1,61 +1,52 @@
+public class PlusPiece extends Piece {
 
-// Model
-
-public class PlusPiece extends Piece{
-
+    // Constructor for PlusPiece
     public PlusPiece(int x, int y, char st, char si) {
-        super(x, y, st, si);   
+        super(x, y, st, si);
     }
 
-    // Test move on fake rules. Remove if necessary.
-
+    // Check if the move to the new position is valid
     @Override
     public boolean validMove(int newX, int newY) {
-
-        int sameX=Math.abs(newX-this.getPosX());
-        int sameY=Math.abs(newY-this.getPosY());
-
-        if(sameY<=3&&sameX==0){
-            return !skipPiece(newX, newY);
-        }else{
-            System.out.println("Horizontal and 3 Tiles only!");
-            return false;
-        }
+        return !skipPiece(newX, newY);
     }
 
+    // Check if there are any pieces in the path to the new position
     @Override
     public boolean skipPiece(int newX, int newY) {
+        // Determine the direction of the move
+        int directionX = Integer.compare(newX, this.getPosX());
+        int directionY = Integer.compare(newY, this.getPosY());
 
-        int directionX=Integer.compare(newX, this.getPosX()); // ke atas x-1, ke bawah x+1
-        int directionY=Integer.compare(newY, this.getPosY()); // ke kanan y+1, ke kiri y-1
+        // Initialize the position to check
+        int checkX = this.getPosX() + directionX;
+        int checkY = this.getPosY() + directionY;
 
-        int checkX=this.getPosX()+directionX; // increment the x position based on the direction
-        int checkY=this.getPosY()+directionY; // increment the y position based on the direction
+        // Loop through each tile in the path
+        while (checkX != newX || checkY != newY) {
+            // Check if the tile is occupied by another piece
+            if (Piece.piecePositions[checkX][checkY] != null) {
+                System.out.println("Tile " + checkX + ", " + checkY + " is occupied. Stopping algorithm.");
+                return true; // There is a piece in the path
+            } else {
+                System.out.println("Tile " + checkX + ", " + checkY + " is NOT occupied");
+            }
+            // Move to the next tile in the path
+            checkX += directionX;
+            checkY += directionY;
+        }
 
-        // System.out.println("Distance X: " + distanceX);
-        // System.out.println("Distance Y: " + distanceY);
-
-        // System.out.println("Destination X: " + newX);
-        // System.out.println("Destination Y: " + newY);
-
-        if(Piece.piecePositions[newX][newY]!=null&&Piece.piecePositions[newX][newY].getSide()!=this.getSide()){ // if ada piece dekat destination AND the piece yang ada dekat situ bukan from the same side of this piece
-            Piece.piecePositions[newX][newY].setStatusDead(); // kill the piece dekat destination
-        }else if(Piece.piecePositions[newX][newY]!=null&&Piece.piecePositions[newX][newY].getSide()==this.getSide()){ // if teammate
-            System.out.println();
+        // If the destination is occupied by an opponent, capture the piece
+        if (Piece.piecePositions[newX][newY] != null && Piece.piecePositions[newX][newY].getSide() != this.getSide()) {
+            Piece.piecePositions[newX][newY].setStatusDead();
+        }
+        // If the destination is occupied by a teammate, disallow the move
+        else if (Piece.piecePositions[newX][newY] != null && Piece.piecePositions[newX][newY].getSide() == this.getSide()) {
             System.out.println("Cannot eat teammate!");
             return true;
         }
 
-        while (checkX!=newX||checkY!=newY) { // selagi belum reach the final destination (newX, newY)
-            if (Piece.piecePositions[checkX][checkY]!=null) { // if the checked tile ada other piece
-                System.out.println("Tile " + checkX + ", " + checkY + " is occupied. Stopping algorithm.");
-                return true;
-            } else {
-                System.out.println("Tile " + checkX + ", " + checkY + " is NOT occupied");
-            }
-            checkX += directionX;
-            checkY += directionY;
-        }
-        return false;
+        return false; // The move is valid
     }
 }
+
