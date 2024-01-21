15bc1a39898555a83a474d372a6ad2e5615b51ff
ASYRANI SYAZWAN BIN YUHANIS <93539057+Asyeerrr@users.noreply.github.com>
2024-01-21 09:11:48 +0800

    Update Movement PointPiece.java
    Point piece can only move 1 or 2 tiles in straight vertical direction

--- PointPiece.java
+++ PointPiece.java
@@ -1,7 +1,4 @@
-
-// Model
-
-public class PointPiece extends Piece{
+public class PointPiece extends Piece {
 
     public PointPiece(int x, int y, char st, char si) {
         super(x, y, st, si);
@@ -9,16 +6,57 @@ public class PointPiece extends Piece{
 
     @Override
     public boolean validMove(int newX, int newY) {
-        if(Piece.piecePositions[newX][newY]==null){ // not occupied tiles
-            return true;
-        }else{
-            return false;
+        int deltaX = Math.abs(newX - getPosX());
+        int deltaY = Math.abs(newY - getPosY());
+        int forward = newX - getPosX();
+
+        // Ensure that the total movement is 1 or 2 tiles
+        int totalMovement = deltaX;
+        char turn = getSide();
+    
+        if (turn == 'Y') {
+            if ((totalMovement == 1 || totalMovement == 2) && deltaY == 0 && forward < 0) {
+                return !skipPiece(newX, newY);
+            } else {
+                System.out.println("You can only move forward 1 or 2 tiles");
+                return false;
+            }
+        } else {
+            if ((totalMovement == 1 || totalMovement == 2) && deltaY == 0 && forward > 0) {
+                return !skipPiece(newX, newY);
+            } else {
+                System.out.println("You can only move forward 1 or 2 tiles");
+                return false;
+            }
         }
     }
 
+
+
     @Override
     public boolean skipPiece(int newX, int newY) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'skipPiece'");
+        int deltaX = Integer.compare(newX, this.getPosX());
+        int deltaY = Integer.compare(newY, this.getPosY());
+
+        int checkX = getPosX() + deltaX;
+        int checkY = getPosY() + deltaY;
+
+        while (checkX != newX || checkY != newY) {
+            if (Piece.piecePositions[checkX][checkY] != null) {
+                System.out.println("Tile " + checkX + ", " + checkY + " is occupied. Stopping algorithm.");
+                return true;
+            }
+            checkX += deltaX;
+            checkY += deltaY;
+        }
+
+        if (Piece.piecePositions[newX][newY] != null && Piece.piecePositions[newX][newY].getSide() != getSide()) {
+            Piece.piecePositions[newX][newY].setStatusDead();
+        } else if (Piece.piecePositions[newX][newY] != null && Piece.piecePositions[newX][newY].getSide() == getSide()) {
+            System.out.println("Cannot eat teammate!");
+            return true;
+        }
+
+        return false;
     }
 }
