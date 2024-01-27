21604994704b8c30023e80831190bea8f5207be2
PennCakeSb <1211104230@student.mmu.edu.my>
2024-01-27 23:05:15 +0800

    added comment

--- PieceMovement.java
+++ PieceMovement.java
@@ -18,7 +18,7 @@ public class PieceMovement {
             
             System.out.println("Placed piece.");
             
-            if(Piece.piecePositions[x][y]!=null){ // if the destination ada other piece
+            if(Piece.piecePositions[x][y]!=null){ // if the has another piece
                 System.out.println("Ate a piece! The piece "+Piece.piecePositions[x][y]+"'s status is DEAD: " + Piece.piecePositions[x][y].getStatus());
                 removePieceFromTile(Piece.piecePositions[x][y]);            
             }
